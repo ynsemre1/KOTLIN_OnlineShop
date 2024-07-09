38b61349c365be56320ea8d3a92e6fcf439a237d
Yunus Emre <kahraman03040506@hotmail.com>
2024-07-10 00:31:23 +0300

    Gradle Extensions Added and Colors Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -46,5 +49,16 @@ dependencies {
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
 
+    //ViewModel
+    implementation("androidx.activity:activity-ktx:1.9.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.tbuonomo:dotsindicator:5.0")
 }
