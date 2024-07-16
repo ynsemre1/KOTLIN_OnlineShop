be1a07d3cff600f88b76fcdd3e5776589dd961e5
Yunus Emre <kahraman03040506@hotmail.com>
2024-07-17 01:27:56 +0300

    Cart Activity UI

---
+++ app/src/main/java/com/example/kotlin_onlineshop/activity/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_onlineshop.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin_onlineshop.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
